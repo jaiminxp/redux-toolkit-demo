658d147dc884cf72fed467ea0f4ad99a9f9f9279
Jaimin Parmar <jaimins100@gmail.com>
2024-09-08 18:20:30 +0530

    fetch dogs data using RTK query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 import { useAppDispatch, useAppSelector } from './app/hooks'
 import { amountAdded, incremented } from './features/counter/counter-slice'
+import { useFetchBreedsQuery } from './features/dogs/dogs-api-slice'
 
 function App() {
   const count = useAppSelector((state) => state.counter.value)
   const dispatch = useAppDispatch()
+  const [numDogs, setNumDogs] = useState(10)
+  const { data = [] } = useFetchBreedsQuery(numDogs)
 
   function handleClick() {
     dispatch(incremented())
@@ -22,13 +26,37 @@ function App() {
           <button onClick={handleClick}>count is {count}</button>
           <button onClick={handleAddAmountClick}>Increment by 5</button>
         </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <select
+          value={numDogs}
+          onChange={(e) => setNumDogs(Number(e.target.value))}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+        <p>Number of dogs fetched: {data.length}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((breed) => (
+              <tr key={breed.id}>
+                <td>{breed.name}</td>
+                <td>
+                  <img src={breed.image.url} alt={breed.name} height={250} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
